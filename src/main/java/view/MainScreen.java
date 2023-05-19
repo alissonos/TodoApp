8e52b7841eea3360f6f185a18724a09492e3f15e
Alisson Oliveira Silva <70547084+alissonos@users.noreply.github.com>
2023-05-19 10:22:49 -0300

    Task de atualizar tarefa.

--- src/main/java/view/MainScreen.java
+++ src/main/java/view/MainScreen.java
@@ -360,6 +360,16 @@ public final class MainScreen extends javax.swing.JFrame {
         taskDialogScreen.setProject(project);
         
         taskDialogScreen.setVisible(true);
+        
+        taskDialogScreen.addWindowListener(new WindowAdapter() {
+            
+            public void windowClosed(WindowEvent e) {
+                
+                int projectindex = jListProjects.getSelectedIndex();
+                Project project = (Project) projectsModel.get(projectindex);
+                loadTasks(project.getId());
+            }
+        });
     }//GEN-LAST:event_jLabelTasksAddMouseClicked
 
     private void jTableTasksMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableTasksMouseClicked
